7ac328d226d7a0025a4781c3c173db5ba35b9360
Andrew Nordman <andrew.nordman@kehe.com>
2011-05-25 15:58:14 -0500

    changed line_end to pull from regex rather than recalculate what we already know

--- plugins/recall/recall.rb
+++ plugins/recall/recall.rb
@@ -10,8 +10,8 @@ class Recall
       m.user.privmsg "Recalling the last #{line_count} lines logged for you:"
     elsif lines =~ /^([0-9]+)-([0-9]+)$/
       line_start = $1.to_i
-      line_count = $2.to_i - line_start + 1
-      line_end = line_start + line_count
+      line_end = $2.to_i
+      line_count = line_end - line_start + 1
       m.user.privmsg "Recalling lines #{line_start}-#{line_end} logged for you:"
     else
       m.privmsg "Unable to interpret line counts for recall, try again."
