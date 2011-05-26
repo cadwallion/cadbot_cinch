9b162c0017f2356780928eefc36bade053680cd1
Andrew Nordman <andrew.nordman@kehe.com>
2011-05-26 15:43:43 -0500

    Added snackcount command, removed cruft

--- plugins/botsnack/bot_snack.rb
+++ plugins/botsnack/bot_snack.rb
@@ -1,9 +1,14 @@
 class BotSnack
   include Cinch::Plugin
-  prefix "@"
-  match "botsnack"
+  match "botsnack", method: :feed
+  match "snackcount", method: :status
   
-  def execute(m)
+  def feed(m)
     m.reply "Thank You! :-)"
+    @bot.database.incr("botsnacks")
+  end
+  
+  def status(m)
+    m.reply "Total snacks eaten: #{@bot.database.get("botsnacks")}"
   end
 end
