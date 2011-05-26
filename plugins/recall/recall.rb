97bf8f740d28a60b91e13a3e3789bdea3caedbf0
Andrew Nordman <andrew.nordman@kehe.com>
2011-05-26 09:15:02 -0500

    Modified the line referencing to redis

--- plugins/recall/recall.rb
+++ plugins/recall/recall.rb
@@ -12,12 +12,12 @@ class Recall
     
     if lines =~ /^([0-9]+)$/
       line_start = 0
-      line_count = $1.to_i
+      line_count = $1.to_i - 1
       m.user.privmsg "Recalling the last #{line_count} lines logged for #{target}:"
     elsif lines =~ /^([0-9]+)-([0-9]+)$/
-      line_start = $1.to_i
-      line_end = $2.to_i
-      line_count = line_end - line_start + 1
+      line_start = $1.to_i - 1
+      line_end = $2.to_i - 1
+      line_count = line_start - line_end
       m.user.privmsg "Recalling lines #{line_start}-#{line_end} logged for #{target}:"
     else
       m.privmsg "Unable to interpret line counts for recall, try again."
