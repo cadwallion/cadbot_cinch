03ff7b756ecae18c55cb2244eb85bef0382a7747
Andrew Nordman <andrew.nordman@kehe.com>
2011-05-27 09:34:44 -0500

    More cleanup of these damn quotes

--- plugins/obedience/obedience.rb
+++ plugins/obedience/obedience.rb
@@ -32,8 +32,13 @@ class Obedience
   def speak(m)
     sources = %w{futurama prog_style subversion joel_on_software starwars calvin math hitchhiker}
     json = Quote.get('/api/v1/random', :query => { :max_lines => "5", :format => "json", :source => sources.join("+") })
-    json['quote'].split(/\n\t/).each do |line|
-      m.reply line.gsub(/\n/,'').gsub(/\t/,'')
+    quote = json['quote']
+    quote.gsub!(/\n\t?\s+-{2}/,'\n--')
+    quote.gsub!(/  /,' ')
+    quote.gsub!(/\\\\n/,'\n')
+    quote.split(/\n/).each do |line|
+      line = line.gsub(/\n/,'').gsub(/\t/,'').lstrip
+      m.reply line unless line == "" 
     end
   end
 end
